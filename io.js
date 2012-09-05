74cc39036a7f44dff0c87cc0380fa8c3febe4aff
macournoyer <macournoyer@gmail.com>
2012-09-05 14:11:33 -0400

    Prep code for demo.

--- io.js
+++
@@ -1,7 +0,0 @@
-var syscalls = require("syscalls");
-
-syscalls.write(1, "hi\n");
-
-/////// EXERCISE ////////
-var data = syscalls.read(0, 10);
-console.log("You typed: " + data)
--- slow.js
+++ slow.js
@@ -8,27 +8,11 @@ http.createServer(function (req, res) {
   if (req.url == "/slow") {
     var objects = [];
     
-    // for (var i=0; i < 10000000; i++) {
-    //   objects.push(new Object()); // pretend we're computing something here
-    // };
-    // res.end("slow request done");
-    
-    ////////////////////////////////////////////////////////////
-    // EXERCISE: use nextTick to unblock the event loop.
-    var i = 0;
-    function compute() {
-      objects.push(new Object());
-      
-      // Schedule another call unless we're done.
-      if (i++ < 1000000) {
-        process.nextTick(compute); // pretend we're computing something here
-      } else {
-        res.end("slow request done");
-      }
-    }
-    compute();
-    ////////////////////////////////////////////////////////////
-    
+    for (var i=0; i < 10000000; i++) {
+      objects.push(new Object()); // pretend we're computing something here
+    };
+    res.end("slow request done");
+        
   } else {
     res.end("fast request done");
     
