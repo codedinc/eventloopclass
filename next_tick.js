039f514d8b1d136d1c81153d35a5a0cf8449ccd9
macournoyer <macournoyer@gmail.com>
2013-02-13 09:04:53 -0500

    Implement nextTick.

--- next_tick.js
+++ next_tick.js
@@ -2,6 +2,9 @@ var loop = require('./loop');
 
 loop.nextTick(function() {
   console.log("This will print last");
+  loop.nextTick(function() {
+    console.log("Another callback");
+  });
 });
 
 console.log("This will print first");
