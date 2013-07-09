357cd921bc3d16cb8105b1d4b7d2dab249bd7032
macournoyer <macournoyer@gmail.com>
2013-07-09 11:20:38 -0400

    Make our server use non-blocking I/O.

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var syscalls = require('syscalls');
 
 var fd = syscalls.socket(syscalls.AF_INET, syscalls.SOCK_STREAM, 0);
+syscalls.fcntl(fd, syscalls.F_SETFL, syscalls.O_NONBLOCK);
 
 syscalls.bind(fd, 3000, "0.0.0.0");
 
 syscalls.listen(fd, 100);
 
 while (true) {
+  syscalls.select([fd], [], []);
   var connFd = syscalls.accept(fd);
   console.log("Accepted new connection");
 
+  syscalls.select([connFd], [], []);
   var data = syscalls.read(connFd, 1024);
   console.log("Received: " + data);
 
+  syscalls.select([], [connFd], []);
   syscalls.write(connFd, "bye!\n");
 
   syscalls.close(connFd);
