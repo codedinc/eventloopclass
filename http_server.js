660d383bfd09cda9445711b6087b69e1af6b14c8
macournoyer <macournoyer@gmail.com>
2013-07-10 10:57:33 -0400

    Make web server send back response.

--- http_server.js
+++ http_server.js
@@ -34,6 +34,14 @@ HttpServer.Connection = function(fd, callback) {
 
   loop.on(fd, 'read', function() {
     var data = syscalls.read(fd, 1024);
+
+    if (data.length == 0) {
+      // Client closed the connection
+      loop.remove(fd, 'read');
+      syscalls.close(fd);
+      return;
+    }
+
     parser.parse(data);
   });
 
@@ -48,13 +56,23 @@ HttpServer.Connection = function(fd, callback) {
 
 HttpServer.Connection.prototype = {
   send: function(body) {
-    // send the response ...
+    var data = "HTTP/1.1 200 OK\r\n" + 
+               "Content-Type: text/plain\r\n" +
+               "Content-Length: " + body.length + "\r\n" +
+               "\r\n" +
+               body;
+
+    var self = this;
+    loop.once(this.fd, 'write', function() {
+      syscalls.write(self.fd, data);
+      syscalls.close(self.fd);
+    });
   }
 }
 
 
 var server = new HttpServer(function(request, response) {
-  response.send("hi!\n");
+  response.send("You requested: " + request.url);
 });
 
 server.listen(3000);
