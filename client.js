3d5c717b154a0f1c498295bfb4e7557c63cc4545
macournoyer <macournoyer@gmail.com>
2014-03-25 14:06:46 -0400

    Implement curl.

---
+++ client.js
@@ -0,0 +1,10 @@
+var syscalls = require('syscalls')
+
+var fd = syscalls.socket(syscalls.AF_INET, syscalls.SOCK_STREAM, 0)
+
+syscalls.connect(fd, 3000, "0.0.0.0")
+
+syscalls.write(fd, "hi\n")
+
+var data = syscalls.read(fd, 1024)
+console.log("Server sent: " + data)
