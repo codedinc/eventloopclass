686d1f794468d07f436cb73d83b249931093a7d3
macournoyer <macournoyer@gmail.com>
2014-03-25 14:17:40 -0400

    Make our server non-blocking.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var syscalls = require('syscalls')
 
 var acceptingFd = syscalls.socket(syscalls.AF_INET, syscalls.SOCK_STREAM, 0)
+syscalls.fcntl(acceptingFd, syscalls.F_SETFL, syscalls.O_NONBLOCK)
 
 syscalls.bind(acceptingFd, 3000, "0.0.0.0")
 
@@ -9,12 +10,15 @@ syscalls.listen(acceptingFd, 100)
 console.log("Listening for connection son port 3000")
 
 while (true) {
+  syscalls.select([acceptingFd], [], [])
   var connectionFd = syscalls.accept(acceptingFd)
   console.log("Accepted new connection")
 
+  syscalls.select([connectionFd], [], [])
   var data = syscalls.read(connectionFd, 1024)
   console.log("Received: " + data)
 
+  syscalls.select([], [connectionFd], [])
   syscalls.write(connectionFd, "bye!\n")
 
   syscalls.close(connectionFd)
